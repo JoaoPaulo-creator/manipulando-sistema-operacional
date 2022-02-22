54c9d84c1482aa8281f9a401cd9caff3869700a5
João Paulo <79981086+JoaoPaulo-creator@users.noreply.github.com>
2022-02-21 22:44:36 -0300

    Add files via upload

---
+++ scripts/Atualizando_pacotes_apt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from time import sleep
+import os
+
+
+def update_packages():
+	update_packages = os.system('sudo apt update')
+	return update_packages
+
+def upgrade_packages():
+	upgrade_packages = os.system('sudo apt upgrade -y')
+	return upgrade_packages
+
+
+def main():
+	update_packages()
+	upgrade_packages()
+	
+	
+if __name__ == '__main__':
+	main()
+
