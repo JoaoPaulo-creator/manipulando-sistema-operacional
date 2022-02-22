83a9481abeeae7ca94aab1d81a83a82617f2a9b3
JoaoPaulo-creator <joaosarto@gmail.com>
2022-02-22 19:46:03 -0300

    Atualizando script

--- scripts/verificando_versao_os.py
+++ scripts/verificando_versao_os.py
@@ -5,15 +5,23 @@ import distro
 Script criado para verificar qual é o nome da distro linux e sua versão do kernel
 '''
 
+
 _system = platform.system()
+
+#versão do kernel
 _kernel_version = platform.release()
+#id da distro
+_id = distro.id()
+#nome da distro
+_name = distro.name()
+#versão da distro
+_version = distro.version()
 
 primeiros_caracteres_kernel_version = list(_kernel_version)
 juntar = ''.join(primeiros_caracteres_kernel_version[0:4])
 
 print(f'Systen: {_system}')
 print(f'Kernel version: {juntar}')
-
-t = distro.linux_distribution()
-print(t[0])
-
+print(f'Distro ID: {_id}')
+print(f'Distro name: {_name}')
+print(f'Distro version: {_version}')
