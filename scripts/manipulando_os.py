779c727b8aa0f8c12098e881a0b8ffa8d97d59b3
João Paulo <79981086+JoaoPaulo-creator@users.noreply.github.com>
2022-02-21 22:33:12 -0300

    script teste

---
+++ scripts/manipulando_os.py
@@ -0,0 +1,66 @@
+from os import makedirs as mk
+from os import removedirs as rd
+from os import system
+from os import chdir as cd
+from time import sleep
+
+def criando_diretorio():
+    try:
+        _mk = mk(f'/home/test/TESTE')    
+        return mk    
+    except FileExistsError:
+        removendo_diretorio()
+
+
+def removendo_diretorio():
+    try:
+        _rd = rd(f'/home/test/TESTE')
+        system('cd ..')
+        return _rd
+    except OSError:
+        return system('rm -r TESTE')
+
+
+def mudando_de_diretorio():
+    _cd = cd('/home/test/TESTE')
+    return _cd
+
+def retornando_dir_antigo():
+    _cd = cd('/home/test')
+    return _cd
+
+def verificando_onde_estou():
+    s = system('pwd')
+    return s
+
+
+def criando_arquivo():
+    with open('teste.txt', 'w') as f:
+        f.write('Se estou lendo isso '
+        'é porque deu certo\n')
+    
+    system('cat teste.txt')
+    return
+
+
+def _ls():
+    return system('ls')
+
+
+def main():
+    criando_diretorio()  
+    sleep(0.5)  
+    _ls()
+    mudando_de_diretorio()        
+    verificando_onde_estou()
+    criando_arquivo()
+    _ls()
+    sleep(2)
+    retornando_dir_antigo()
+    removendo_diretorio()
+    _ls()
+
+
+
+if __name__=='__main__':
+    main()    
